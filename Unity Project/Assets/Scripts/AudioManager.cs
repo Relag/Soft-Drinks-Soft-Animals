bede7362ef1f62cf975fa3ce0d97ea76f8e71d54
Garguth <just7166@gmail.com>
2023-02-21 00:16:33 -0500

    Revert "idk why Unity is having me push everything"
    This reverts commit ab34976ba16b092a8640cf182a243e8423f223b5.

---
+++ Unity Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+    
+    // Singleton pattern to ensure only one AudioManager exists at the same time.
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
