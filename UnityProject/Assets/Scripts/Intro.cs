088d2edeafaf3a00c8b4a78d431a28f780796703
Relag <rejean.lagasse93@gmail.com>
2023-02-27 22:06:29 -0500

    Update Intro.cs
    Removed unnecessary functionality for multiple intro text boxes.

--- UnityProject/Assets/Scripts/Intro.cs
+++ UnityProject/Assets/Scripts/Intro.cs
@@ -15,12 +15,6 @@ public class Intro : MonoBehaviour
     private string[] textboxes;
     private int TEXTBOXNUM = 3;
     private int currentTextBox = 0;
-
-    private void Start() {
-        textboxes[0] = "It’s a bright, clear morning in plush Bermodo City. The sun is shining, the clouds are stuffing-soft, and you are...why are you here again?\n\n...That’s right. Aunt Dora, the old duck, has given you her old soda shop. You don’t really remember her. Your mom certainly didn’t talk about her. But, hey, you’ve always wanted to move to the big city.\n\nAnd if Aunt Dora could do it, how hard can running a soda shop be?";
-        introText.text = textboxes[currentTextBox];
-    }
-
  
 
     public void moveForward() {
