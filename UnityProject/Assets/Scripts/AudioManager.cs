4d8c4d9f775e97bba416c0876edcfd01866dccde
Relag <rejean.lagasse93@gmail.com>
2023-02-27 19:11:05 -0500

    Added SFX audio clips
    Added the SFX clips to the AudioManager. Added functions for calling the clips.

--- UnityProject/Assets/Scripts/AudioManager.cs
+++ UnityProject/Assets/Scripts/AudioManager.cs
@@ -8,7 +8,14 @@ public class AudioManager : MonoBehaviour
     public static AudioManager instance;
     public AudioSource bgmSource;
     public AudioSource sfxSource;
-    public AudioClip[] audioClips;
+    public AudioClip[] musicClips;
+    public AudioClip[] garbageClips;
+    public AudioClip[] sodaClips;
+    public AudioClip[] bookClips;
+    public AudioClip[] animalClips;
+    public AudioClip[] orderComplete;
+    private bool audioOn = true;
+    private bool sfxOn = true;
     
     // Singleton pattern to ensure only one AudioManager exists at the same time.
     void Awake()
@@ -24,15 +31,51 @@ public class AudioManager : MonoBehaviour
     }
 
     private void Start() {
-        bgmSource.clip = audioClips[0];
+        bgmSource.clip = musicClips[0];
         bgmSource.loop = true;
         bgmSource.Play();
     }
 
-    //Call this when you want to change the song. Check Audiomanager for track listings.
+    //Call this when you want to change the song. Track  Listings:
+    //0: Main Menu
+    //1: Gameplay Loop
+    //2: Happy String
+    //3: Sad String
     public void changeSong(int track) {
-        if (track < audioClips.Length)
-            bgmSource.clip = audioClips[track];
-        bgmSource.Play();
+        if (audioOn) {
+            if (track < musicClips.Length)
+                bgmSource.clip = musicClips[track];
+            bgmSource.Play();
+        }
+    }
+
+    public void garbageSFX() {
+        if (sfxOn)
+            sfxSource.PlayOneShot(garbageClips[Random.Range(0, garbageClips.Length)]);
+    }
+
+    public void bookSFX() {
+        if (sfxOn)
+            sfxSource.PlayOneShot(bookClips[Random.Range(0, bookClips.Length)]);
+    }
+
+    public void sodaSFX() {
+        if (sfxOn) {
+            sfxSource.PlayOneShot(sodaClips[0]);
+            sfxSource.PlayOneShot(sodaClips[Random.Range(1, sodaClips.Length)]);
+        }
+    }
+
+    public void stopSodaSFX() {
+        sfxSource.Stop();
+    }
+
+    public void orderCompleteSFX(bool result) {
+        if (sfxOn) {
+            if (result)
+                sfxSource.PlayOneShot(orderComplete[0]);
+            else
+                sfxSource.PlayOneShot(orderComplete[1]);
+        }
     }
 }
