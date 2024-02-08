48500b9e054d3d0a8a159950c2a5237d1fb880e7
Relag <rejean.lagasse93@gmail.com>
2024-02-08 16:01:58 -0500

    updated Customer.cs
    Made variables protected. Added getter-setters where necessary. Added function to retrieve dialogue, we'll see if it works.

--- UnityProject/Assets/Scripts/Customers/Customer.cs
+++ UnityProject/Assets/Scripts/Customers/Customer.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Customer { 
 
     //A dictionary of dictionaries. Stores day number as key to nested dictionaries, bool true for correct order, false for incorrect order, undefined for intro dialogue. 
-    public Dictionary<int, Dictionary <bool, string>> dialogue = new Dictionary<int, Dictionary<bool, string>>();
-    Drink customerDrink = new Drink();
-    string name;
-    Sprite customerSprite;
+    protected Dictionary<int, Dictionary <bool, string>> dialogue = new Dictionary<int, Dictionary<bool, string>>();
+    protected Drink customerDrink { get; }
+    protected string name { get; }
+    protected Sprite customerSprite { set; get; }
+
+    public string GetDialogue (int day, bool response) {
+        string speech;
+        Dictionary<bool, string> whatDay = new Dictionary<bool, string>();
+        whatDay = dialogue[day];
+        speech = whatDay[response];
+        return speech;
+    }
 }
