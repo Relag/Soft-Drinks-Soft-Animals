4fd6f5102dee4c080a051e2d87216d0b0d6b9d70
Relag <rejean.lagasse93@gmail.com>
2024-04-29 15:36:18 -0400

    Update Localizer.cs
    Made Localizer a singleton class. Made a function to access the dictionary values.

--- UnityProject/Assets/Scripts/Events/Localizer.cs
+++ UnityProject/Assets/Scripts/Events/Localizer.cs
@@ -6,31 +6,46 @@ using System.IO;
 
 public enum LANGUAGE { ENGLISH, FRENCH };
 
-public class Localizer {
+public sealed class Localizer {
     Dictionary<string, string> LocalizedText = new Dictionary<string, string>();
     string stream = "../UnityProject/Assets/Resources/Text/GameText.csv";
 
-    public LANGUAGE gameLanguage = LANGUAGE.ENGLISH;
+    public LANGUAGE gameLanguage;
 
-    public void GetLocalizedText() {
+    private Dictionary<string, string> currentLanguage = new Dictionary<string, string>()
+
+    private static readonly Localizer instance = new Localizer();
+
+    static Localizer() { }
+
+    private Localizer() {
+        GetLocalizedText(LANGUAGE.ENGLISH);
+    }
+
+    public static Localizer Instance {
+        get { return instance; }
+    }
+
+    //Function reads the .csv file and puts the requested language a dictionary
+    //This dictionary is then called by text objects to determine what text should be displayed.
+    //Should only be called at start of game, and when changing language.
+    public void GetLocalizedText(LANGUAGE language) {
+        gameLanguage = language;
     
     using var reader = new StreamReader(stream);
         var csv = new CSVReader();
-        var result = csv.ParseCSV(File.ReadAllText(stream), gameLanguage);
+
+        currentLanguage.Clear();
+        currentLanguage = csv.ParseCSV(File.ReadAllText(stream), gameLanguage);
 
         int i = 0;
 
         foreach(var line in result) {
             Debug.Log(line.Key.ToString() + " : " + line.Value.ToString());
         }
+    }
 
-        /*while (csv.Read()) {
-            LocalizedText.Add(csv.GetField("Key"), csv.GetField(language));
-        }
-
-        foreach (KeyValuePair<string, string> text in LocalizedText) {
-            Debug.Log(text.Key + " : " + text.Value);
-        }*/
-      //  reader.ReadHeaders();
-}
+    public string getText(string key) {
+        return currentLanguage[key];
+    }
 }
